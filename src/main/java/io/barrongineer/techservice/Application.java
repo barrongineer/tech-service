994694a88fbea8645a841050136a7f2394bb6134
shaunnbarron <shaunnb@gmail.com>
2015-04-12 02:30:32 -0400

    added oauth.

--- src/main/java/io/barrongineer/techservice/Application.java
+++ src/main/java/io/barrongineer/techservice/Application.java
@@ -2,11 +2,13 @@ package io.barrongineer.techservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 /**
  * Created by shaunn on 3/29/2015.
  */
 @SpringBootApplication
+@EnableOAuth2Resource
 public class Application {
 
     public static void main(String[] args) {
