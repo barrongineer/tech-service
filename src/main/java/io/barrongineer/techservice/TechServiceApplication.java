f68c03f863b110321872113e503114db8e226f39
shaunnbarron <shaunnb@gmail.com>
2015-04-12 05:37:04 -0400

    added eureka service registration/discovery.

--- src/main/java/io/barrongineer/techservice/TechServiceApplication.java
+++ src/main/java/io/barrongineer/techservice/TechServiceApplication.java
@@ -2,13 +2,15 @@ package io.barrongineer.techservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 /**
  * Created by shaunn on 3/29/2015.
  */
-@SpringBootApplication
+@EnableEurekaClient
 @EnableOAuth2Resource
+@SpringBootApplication
 public class TechServiceApplication {
 
     public static void main(String[] args) {
